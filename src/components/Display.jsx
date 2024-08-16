85256799021cf468d665f35110b4980ba757c1fc
MADHAN79 <madhanvarshini3@gmail.com>
2024-08-16 19:59:13 +0530

    Navbar Completed

---
+++ src/components/Display.jsx
@@ -0,0 +1,19 @@
+//in this component we'll display the home & albums
+
+import { Route, Routes } from "react-router-dom"
+import DisplayHome from "./DisplayHome"
+
+const Display = () => {
+  return (
+    <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
+
+        {/* we are creating Routes for the HOME & ALBUMS page */}
+        <Routes>
+            {/* path="/" is LANDING PAGE,here it displays DisplayHome component*/}
+            <Route path="/" element={<DisplayHome />}  />
+        </Routes>
+    </div>
+  )
+}
+
+export default Display
---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar"
+
+
+const DisplayHome = () => {
+  return (
+    <>
+     {/* Here we have mounted this Navbar component inside this DisplayHome component */}
+     <Navbar />
+    </>
+  )
+}
+
+export default DisplayHome
