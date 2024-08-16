03f891a4485a4e65d5e163850d7d067f9dacaa6f
MADHAN79 <madhanvarshini3@gmail.com>
2024-08-16 22:17:04 +0530

    Home page half way done

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,4 +1,6 @@
 import Navbar from "./Navbar"
+import { albumsData } from "../assets/assets"
+import AlbumItem from './AlbumItem'
 
 
 const DisplayHome = () => {
@@ -6,6 +8,29 @@ const DisplayHome = () => {
     <>
      {/* Here we have mounted this Navbar component inside this DisplayHome component */}
      <Navbar />
+     
+     <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        
+        {/* overflow-auto will display a horzontal bar if the screen size got reduced & this applied only for this div and not for the whole website :) | but we can still use the horizontal slider by hiding this scroll bar*/}
+        {/* see that magic sliding effect in index.css */}
+        <div className="flex overflow-auto">
+            {/* passing each item & its index | this will loop through all items in albumsData array in in assets.js in assets folder*/}
+            {albumsData.map((item, index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+        </div>
+     </div>
+
+     <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+        
+        {/* overflow-auto will display a horzontal bar if the screen size got reduced & this applied only for this div and not for the whole website :) | but we can still use the horizontal slider by hiding this scroll bar*/}
+        {/* see that magic sliding effect in index.css */}
+        <div className="flex overflow-auto">
+            {/* passing each item & its index | this will loop through all items in albumsData array in in assets.js in assets folder*/}
+            {albumsData.map((item, index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+        </div>
+     </div>
+
     </>
   )
 }
